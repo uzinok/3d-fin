5699d10c4f9229d3a6cd772337b24c4e6dbf4cbe
uzinok <johndoe@example.com>
2025-09-20 01:28:06 +0300

    начинает делать админку

--- src/components/pages/admin/admin.js
+++ src/components/pages/admin/admin.js
@@ -1,15 +1,23 @@
-import Index from "../../layout/index";
-// import { StyledContainer } from "./styles";
-import HeroEdit from "../../blocks/hero/hero-edit/hero-edit";
-// import EditGallery from "../../blocks/gallery/edit-gallery/edit-gallery";
+import Header from "../../layout/header/header";
+import { StyledContainer, StyledLink, StyledContent } from "./styles";
+import Title from "../../ui/title/title";
 
 function Admin() {
 
 	return (
-		<Index>
-			<HeroEdit />
-			{/* <EditGallery /> */}
-		</Index>
+		<>
+			<Header>
+				<StyledLink to="/">Главная</StyledLink>
+				<button>Выход</button>
+			</Header>
+			<StyledContainer>
+				<Title>
+					Добро пожаловать в панель администратора!
+				</Title>
+			</StyledContainer>
+			<StyledContent>
+			</StyledContent>
+		</>
 	);
 }
 
--- src/components/pages/admin/styles.js
+++ src/components/pages/admin/styles.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import Container from "../../layout/container/container";
+import { Link } from "react-router-dom";
 
 export const StyledContainer = styled(Container)`
 	padding-top: 65px;
+`
+
+export const StyledContent = styled.div`
+	background: #ffffff;
+`
+
+export const StyledLink = styled(Link)`
+	color: inherit;
 `
