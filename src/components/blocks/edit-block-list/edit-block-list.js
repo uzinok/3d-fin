39e1bfe9ca97d360ce9ed188f98d9e9ff7332cae
uzinok <41639106+uzinok@users.noreply.github.com>
2025-09-23 15:22:15 +0300

    добавляет editCard в edit-block-list

--- src/components/blocks/edit-block-list/edit-block-list.js
+++ src/components/blocks/edit-block-list/edit-block-list.js
@@ -6,14 +6,58 @@ import TextAreaForEdit from "../../ui/form-elements/textarea-for-edit/textarea-f
 import { useRef } from "react";
 import { StyledList } from "./styles";
 
-function EditBlockList({ data }) {
-	const titleRef = useRef(null);
-	const subTitleRef = useRef(null);
+function EditCard({ key, data }) {
 	const iconRef = useRef(null);
 	const cardTitleRef = useRef(null);
 	const cardDescriptionRef = useRef(null);
 	const cardHrefRef = useRef(null);
 	const cardLinkTextRef = useRef(null);
+	return (
+		<li key={key}>
+			<form>
+				<p> icon: {data.icon}
+					<TextAreaForEdit
+						ref={iconRef}
+						defaultValue={data.icon}
+						noMinHeight="true"
+					/>
+				</p>
+				<p>title:
+					<TextAreaForEdit
+						ref={cardTitleRef}
+						defaultValue={data.title}
+						noMinHeight="true"
+					/>
+				</p>
+				<p>description:
+					<TextAreaForEdit
+						ref={cardDescriptionRef}
+						defaultValue={data.description}
+						noMinHeight="true"
+					/>
+				</p>
+				<p>href:
+					<TextAreaForEdit
+						ref={cardHrefRef}
+						defaultValue={data.href}
+						noMinHeight="true"
+					/>
+				</p>
+				<p>linkText:
+					<TextAreaForEdit
+						ref={cardLinkTextRef}
+						defaultValue={data.linkText}
+						noMinHeight="true"
+					/>
+				</p>
+			</form>
+		</li>
+	)
+}
+
+function EditBlockList({ data }) {
+	const titleRef = useRef(null);
+	const subTitleRef = useRef(null);
 
 	return (
 		<section>
@@ -36,45 +80,13 @@ function EditBlockList({ data }) {
 				)}
 				{data.list && (
 					<StyledList>
-					{data.list.map((item, index) => (
-						<li key={index}>
-							<p key={index}>icon: {item.icon}
-								<TextAreaForEdit
+						{data.list.map((item, index) => (
+							<>
+								<EditCard
 									key={index}
-									ref={iconRef}
-									defaultValue={item.icon}
-									noMinHeight="true"
+									data={item}
 								/>
-							</p>
-							<p>title:
-								<TextAreaForEdit
-									ref={cardTitleRef}
-									defaultValue={item.title}
-									noMinHeight="true"
-								/>
-							</p>
-							<p>description:
-								<TextAreaForEdit
-									ref={cardDescriptionRef}
-									defaultValue={item.description}
-									noMinHeight="true"
-								/>
-							</p>
-							<p>href:
-								<TextAreaForEdit
-									ref={cardHrefRef}
-									defaultValue={item.href}
-									noMinHeight="true"
-								/>
-							</p>
-							<p>linkText:
-								<TextAreaForEdit
-									ref={cardLinkTextRef}
-									defaultValue={item.linkText}
-									noMinHeight="true"
-								/>
-							</p>
-						</li>
+							</>
 					))}
 					</StyledList>
 				)}
