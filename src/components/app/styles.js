b9bd4b790e00d98f098c9b5705682cc982ddde1c
uzinok <johndoe@example.com>
2025-09-28 03:37:30 +0300

    переделывает шрифты

--- src/components/app/styles.js
+++ src/components/app/styles.js
@@ -1,42 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import RobotoRegular from "../../assets/fonts/Roboto-Regular.woff2"
-import RobotoCondensedSemiBold from "../../assets/fonts/Roboto_Condensed-SemiBold.woff2"
-import RobotoSemiBold from "../../assets/fonts/Roboto-SemiBold.woff2"
-import RobotoBold from "../../assets/fonts/Roboto-Bold.woff2"
 
 export const GlobalStyle = createGlobalStyle`
-
-@font-face {
-	font-family: 'Roboto';
-	font-weight: 400;
-	font-style: normal;
-	font-display: swap;
-	src: url(${RobotoRegular}) format('woff2');
-}
-
-@font-face {
-	font-family: 'Roboto';
-	font-weight: 600;
-	font-style: normal;
-	font-display: swap;
-	src: url(${RobotoSemiBold}) format('woff2');
-}
-
-@font-face {
-	font-family: 'Roboto';
-	font-weight: 700;
-	font-style: normal;
-	font-display: swap;
-	src: url(${RobotoBold}) format('woff2');
-}
-
-@font-face {
-	font-family: "Roboto Condensed";
-	font-weight: 600;
-	font-style: normal;
-	font-display: swap;
-	src: url(${RobotoCondensedSemiBold}) format('woff2');
-}
 
 html,
 body {
@@ -52,7 +16,7 @@ body {
 	min-width: 320px;
 	margin: 0;
 	line-height: 1.3;
-	font-family: "Roboto", Arial, Helvetica, sans-serif;
+	font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 	color: #ffffff;
 	background-color: #1f1f1f;
 	text-size-adjust: 100%;
